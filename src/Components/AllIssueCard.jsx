4333885c26ed810710399626fc388824915cb07a
lahifkhan <lahifkhan52@gmail.com>
2025-11-10 14:26:08 +0600

    all issue page with filter done

---
+++ src/Components/AllIssueCard.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { CiLocationOn } from "react-icons/ci";
+import { FaDonate } from "react-icons/fa";
+
+const AllIssueCard = ({ issue }) => {
+  const { title, description, category, location, image, amount } = issue;
+  return (
+    <div className="card bg-base-100  shadow-2xl  hover:scale-105 transition-transform duration-300">
+      <figure className="w-full h-56 overflow-hidden">
+        <img
+          src={image}
+          alt={title}
+          className="w-full h-full object-cover hover:scale-105 transition-transform duration-300 "
+        />
+      </figure>{" "}
+      <div className="card-body">
+        <h2 className="card-title font-bold text-xl">
+          {title}
+          <div className="badge badge-secondary">NEW</div>
+        </h2>
+        <p className="text-secondary">{description}</p>
+        <div className="card-actions justify-end flex flex-col">
+          <div className="badge badge-dash badge-primary">{category}</div>
+          <div className="badge badge-dash badge-primary">
+            <CiLocationOn />
+            {location}
+          </div>
+        </div>
+        <div className="flex justify-between items-center ">
+          <button className="btn btn-secondary">See Details</button>
+          <div className="flex items-center gap-1 font-bold badge badge-outline">
+            <FaDonate />
+            {amount}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AllIssueCard;
--- src/Pages/AllIssue.jsx
+++ src/Pages/AllIssue.jsx
@@ -1,24 +1,102 @@
 import React, { useEffect, useState } from "react";
 import useAxiosInstance from "../Hook/useAxiosInstance";
+import AllIssueCard from "../Components/AllIssueCard";
+import { IoCheckmarkDoneCircleOutline } from "react-icons/io5";
+import { MdCategory, MdClear, MdOutlinePendingActions } from "react-icons/md";
 
 const AllIssue = () => {
   const [allIssue, setAllIssue] = useState([]);
   const [loading, setloading] = useState(false);
+  const [category, setCategory] = useState("");
+  const [status, setStatus] = useState("");
   const axiosInstance = useAxiosInstance();
 
   useEffect(() => {
     setloading(true);
-    axiosInstance.get("/issues").then((data) => {
-      console.log(data.data);
+
+    let url = "/filterIssue?";
+    if (category) {
+      url += `category=${category}&`;
+    }
+    if (status) {
+      url += `status=${status}`;
+    }
+
+    axiosInstance.get(url).then((data) => {
       setAllIssue(data.data);
       setloading(false);
     });
-  }, []);
+  }, [category, status, axiosInstance]);
+
+  const handleClear = () => {
+    setCategory("");
+    setStatus("");
+  };
 
   if (loading) {
     return <p>loading...</p>;
   }
-  return <div></div>;
+  return (
+    <div className="mb-8 w-11/12 mx-auto">
+      <h1 className="text-center font-bold text-2xl md:text-5xl text-primary mt-20 mb-8">
+        All Issues
+      </h1>
+      <div>
+        <div>
+          <div className="flex flex-wrap items-center justify-end gap-3 mb-6 p-3 bg-base-100 shadow-md rounded-lg">
+            {/* Category Filter */}
+            <div className="flex items-center gap-2">
+              <MdCategory className="text-primary text-xl" />
+              <select
+                onChange={(e) => setCategory(e.target.value)}
+                value={category}
+                className="select select-bordered select-sm rounded-full"
+              >
+                <option value="">All Categories</option>
+                <option value="Garbage"> Garbage</option>
+                <option value="Illegal Construction">
+                  Illegal Construction
+                </option>
+                <option value="Broken Public Property">
+                  Broken Public Property
+                </option>
+                <option value="Road Damage">Road Damage</option>
+              </select>
+            </div>
+
+            {/* Status Filter */}
+            <div className="flex gap-2 items-center">
+              <MdOutlinePendingActions className="text-primary text-xl" />
+              <select
+                onChange={(e) => setStatus(e.target.value)}
+                value={status}
+                className="select select-bordered select-sm rounded-full"
+              >
+                <option value="">All Status</option>
+                <option value="ongoing">Ongoing</option>
+                <option value="resolved">Resolved</option>
+              </select>
+            </div>
+
+            <div className="flex gap-1">
+              <button
+                onClick={handleClear}
+                className="btn btn-sm btn-outline rounded-full"
+              >
+                <MdClear />
+                Clear Filters
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        {allIssue.map((issue) => (
+          <AllIssueCard issue={issue}></AllIssueCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AllIssue;
